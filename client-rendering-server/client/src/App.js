afe09fdc2582ec303abbe2438677dd734cb75578
ginsenglee <thecynicdog0328@gmail.com>
2024-01-21 13:01:14 +0900

    Feat: added signup type separation

--- client-rendering-server/client/src/App.js
+++ client-rendering-server/client/src/App.js
@@ -1,26 +1,38 @@
-import { Route, Routes } from 'react-router';
-import { Layout } from './layout/Layout'
+import { Route, Routes, useParams } from 'react-router-dom';
+import { Layout } from './layout/Layout';
 import { Component } from "react";
 import { Users } from "./component/Users";
 import { Home } from "./component/Home";
 import { SingupEntry } from "./component/signup/Singup-entry";
 import { SignupStudent } from "./component/signup/Signup-student";
+import { SignupTeacher } from "./component/signup/Signup-teacher"; // Import the SignupTeacher component
 
 export default class App extends Component {
-
     render() {
         return (
             <Layout>
                 <Routes>
                     {["/home", "/"].map((path, index) =>
-                        <Route path={path} element={<Home/>} key={index} />
+                        <Route path={path} element={<Home />} key={index} />
                     )}
                     <Route path='/users' element={<Users />} />
                     <Route path='/signup' element={<SingupEntry />} />
-                    <Route path='/signup/student' element={<SignupStudent />} />
+                    {/* Use the useSignupRoute function to render the appropriate Signup component */}
+                    <Route path='/signup/:type' element={<SignupRouter />} />
                 </Routes>
             </Layout>
-        )
+        );
     }
-
 }
+
+const SignupRouter = () => {
+    const { type } = useParams();
+
+    if (type === 'student') {
+        return <SignupStudent type={type} />;
+    } else if (type === 'teacher') {
+        return <SignupTeacher type={type} />;
+    } else {
+        return <div>Invalid sign-up type</div>;
+    }
+};
