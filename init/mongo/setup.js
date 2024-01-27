09d1f60253e7716daeebd720ae50330e97f8d85c
ginsenglee <thecynicdog0328@gmail.com>
2024-01-27 22:36:13 +0900

    Chore: added house table populating query

--- init/mongo/setup.js
+++ init/mongo/setup.js
@@ -1,3 +1,28 @@
 db.createCollection("user");
 db.user.createIndex({username: 1}, {unique: true});
 db.user.createIndex({emailAddress: 1}, {unique: true});
+
+db.createCollection("house");
+db.house.insertMany([
+    {
+        title: "Gryffindor",
+        quota: 100,
+        points: 500,
+    },
+    {
+        title: "Slytherin",
+        quota: 90,
+        points: 480,
+    },
+    {
+        title: "Ravenclaw",
+        quota: 95,
+        points: 460,
+    },
+    {
+        title: "Hufflepuff",
+        quota: 80,
+        points: 420,
+    },
+]);
+db.house.createIndex({ title: 1 }, { unique: true });
