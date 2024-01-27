c06bdeb978dbb43972b25fc5532bd93dd562216f
ginsenglee <thecynicdog0328@gmail.com>
2024-01-27 16:30:29 +0900

    Chore: modified position

--- user-service/src/main/java/org/example/util/PhotoGenerator.java
+++ user-service/src/main/java/org/example/util/PhotoGenerator.java
@@ -9,22 +9,53 @@ import java.util.Random;
 
 public class PhotoGenerator {
 
+    private static final Color[] PASTEL_COLORS = {
+            hexToColor("8db150"),
+            hexToColor("c99f88"),
+            hexToColor("bca78a"),
+            hexToColor("3d3d3d"),
+            hexToColor("477391"),
+            hexToColor("665987"),
+            hexToColor("42663a"),
+            hexToColor("b33360"),
+            hexToColor("cf7684"),
+            hexToColor("94b79b"),
+            hexToColor("d5a5a0"),
+            hexToColor("747635")
+    };
+
+    private static Color hexToColor(String hex) {
+        return new Color(
+                Integer.valueOf(hex.substring(0, 2), 16),
+                Integer.valueOf(hex.substring(2, 4), 16),
+                Integer.valueOf(hex.substring(4, 6), 16)
+        );
+    }
+
     public static String generateUserProfile(String username) throws IOException {
-        int imageSize = 100;
+        int imageSize = 50;
         BufferedImage image = new BufferedImage(imageSize, imageSize, BufferedImage.TYPE_INT_RGB);
         Graphics2D graphics = image.createGraphics();
 
+        // Randomly choose a pastel color
         Random random = new Random();
-        Color backgroundColor = new Color(random.nextInt(256), random.nextInt(256), random.nextInt(256));
+        Color backgroundColor = PASTEL_COLORS[random.nextInt(PASTEL_COLORS.length)];
+
         graphics.setColor(backgroundColor);
         graphics.fillRect(0, 0, imageSize, imageSize);
 
         graphics.setColor(Color.WHITE); // You can customize the text color
         graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        graphics.setFont(new Font("SansSerif", Font.BOLD, 30)); // You can customize the font and size
-        int x = (imageSize - graphics.getFontMetrics().stringWidth(username)) / 2;
-        int y = imageSize / 2;
-        graphics.drawString(username.substring(0, 1), x, y);
+        graphics.setFont(new Font("SansSerif", Font.PLAIN, 30)); // You can customize the font and size
+
+        FontMetrics fontMetrics = graphics.getFontMetrics();
+        int stringWidth = fontMetrics.stringWidth(username.substring(0, 1));
+        int stringHeight = fontMetrics.getHeight();
+
+        float x = (imageSize - stringWidth) / 2f;
+        float y = (imageSize - stringHeight) / 2f + fontMetrics.getAscent(); // Adjust y to include ascent
+
+        graphics.drawString(username.substring(0, 1), x + .9f, y - 1.3f);
 
         graphics.dispose();
 
