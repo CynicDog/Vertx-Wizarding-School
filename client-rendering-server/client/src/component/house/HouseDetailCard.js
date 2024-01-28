9da07ea7af52cfe0e0f604e90bd14edef3cf1d93
ginsenglee <thecynicdog0328@gmail.com>
2024-01-28 12:05:21 +0900

    Style: added user avatars

--- client-rendering-server/client/src/component/house/HouseDetailCard.js
+++ client-rendering-server/client/src/component/house/HouseDetailCard.js
@@ -1,4 +1,8 @@
-const HouseDetailCard = ({house}) => {
+import AvatarOther from "../user-page/AvatarOther";
+import {useEffect} from "react";
+
+const HouseDetailCard = ({house, users}) => {
+
     if (!house) {
         return <div>Loading...</div>;
     }
@@ -6,19 +10,22 @@ const HouseDetailCard = ({house}) => {
     return (
         <>
             <div className="card card-cover border-0 rounded-4 shadow-sm">
-                <div className="d-flex flex-column p-5 pb-3">
-                    <div className="d-flex justify-content-between">
-                        <div>
-                            <div className="fw-lighter my-2">house rank</div>
-                        </div>
-                        <div>
-                            <div className="btn btn-light btn-sm">enroll</div>
+                <div className="d-flex flex-column p-3">
+                    <div className="d-flex justify-content-between my-2">
+                        <div className="text-black fw-bold fs-3">
+                            <span className="mx-3">{house.title}</span>
+                            <AvatarOther username={house.head}/>
                         </div>
                     </div>
                     <div>
-                        <h3 className="display-6 text-black fw-bold fs-3 pt-4 my-4">{house.title}</h3>
-                        {/* some contents .. */}
-                        <div>
+                        <div className="border rounded p-3 m-2">
+                            <span className="fw-lighter fs-5">People</span>
+                            <hr className="mt-1 mb-2" />
+                            <div>
+                                {users && users.map((user, index) => (
+                                    <AvatarOther key={index} username={user.username} />
+                                ))}
+                            </div>
                         </div>
                     </div>
                 </div>
